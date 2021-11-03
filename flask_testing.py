cdf566e5bc8a7b7511950a1dad0c7800b215eecd
villar10 <60012351+villar10@users.noreply.github.com>
2021-11-03 15:34:04 -0400

    More Flask testing, removed Connexion as it will no longer be used

--- flask_testing.py
+++ flask_testing.py
@@ -1,23 +1,22 @@
-from flask import render_template
-import connexion
+from flask import (
+    Flask,
+    render_template
+)
 
 # Create the application instance
-options = {"swagger_ui": False}
-app = connexion.App(__name__, specification_dir='./', options=options)
-
-# Read the swagger.yml file to configure the endpoints
-app.add_api('ECE_461_Fall_2021_Project_2_spec_v1.0.yaml')
+app = Flask(__name__, template_folder="templates")
 
 # Create a URL route in our application for "/"
-@app.route('/')
+@app.route('/login')
 def home():
     """
-    This function just responds to the browser URL
+    This function just responds to the browser ULR
     localhost:5000/
+
     :return:        the rendered template 'home.html'
     """
     return render_template('home.html')
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)

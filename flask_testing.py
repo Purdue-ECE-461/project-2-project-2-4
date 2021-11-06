386c9f9df0ea7a347aa79e458f52d937c5eb0743
villar10 <60012351+villar10@users.noreply.github.com>
2021-11-06 15:29:48 -0400

    testing flask functions

--- flask_testing.py
+++ flask_testing.py
@@ -7,7 +7,7 @@ from flask import (
 app = Flask(__name__, template_folder="templates")
 
 # Create a URL route in our application for "/"
-@app.route('/login')
+@app.route('/')
 def home():
     """
     This function just responds to the browser ULR
@@ -16,6 +16,19 @@ def home():
     :return:        the rendered template 'home.html'
     """
     return render_template('home.html')
+
+
+
+@app.route('/<int:number>/')
+def incrementer(number):
+    return "Incremented number is INT " + str(number+1)
+@app.route('/<int:number>/')
+def incrementer2(number):
+    return "Incremented number is FLOAT " + str(number+1)
+
+@app.route('/<string:name>/')
+def hello(name):
+    return "Hello " + name
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':

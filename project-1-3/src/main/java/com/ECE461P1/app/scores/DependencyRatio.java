d09c0aeafb594dd916f86722e67fb49ad1ee03f0
Owen Prince <oprince@purdue.edu>
2021-11-05 23:09:24 -0400

    Finish basic json parsing
    Start working on dependency ratio, csvFileSource does not work
    Switch to other source

--- project-1-3/src/main/java/com/ECE461P1/app/scores/DependencyRatio.java
+++ project-1-3/src/main/java/com/ECE461P1/app/scores/DependencyRatio.java
@@ -1,11 +1,12 @@
 package com.ECE461P1.app.scores;
+import java.io.FileReader;
+import javax.json.Json;
+import javax.json.JsonReader;
+import javax.json.JsonStructure;
 import com.ECE461P1.app.scores.Score;
 import org.apache.commons.io.IOUtils;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.io.*;
-//import java.io.InputStream;
+
 
 public class DependencyRatio extends Score{
 
@@ -13,54 +14,27 @@ public class DependencyRatio extends Score{
         super(_owner, _repo);
     }
 
-//    public HttpURLConnection makeHttpConnection() throws java.io.IOException{
-//        URL gQLUrl = new URL("https://api.github.com/graphql");
-//        int respon = httpreq(gQLUrl);
-//        HttpURLConnection conn = (HttpURLConnection) gQLUrl.openConnection();
-//        conn.setRequestMethod("POST");
-//        conn.setDoOutput(true);
-//        conn.setDoInput(true);
-//        conn.setRequestProperty("Authorization", "token " + System.getenv("GITHUB_TOKEN"));
-//        conn.setRequestProperty("Accept", "application/vnd.github.hawkgirl-preview+json");
-//        conn.setRequestProperty("Content-Type", "application/json");
-////        {repository(owner: "node-facebook", name: "facebook-node-sdk") {description}}
-//
-//        DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
-////        wr.writeBytes("{\"query\":\"query{search(type:USER query:\"location:lagos language:java\"){userCount}}}");
-//        wr.writeBytes("{\"query\":{\nrepository(owner: \"node-facebook\", name: \"facebook-node-sdk\"){\ndescription\n}\n}");
-//
-//
-//        wr.flush();
-//        wr.close();
-//
-//        String jsonResponse = "";
-//        java.io.InputStream inputStream = null;
-//
-//        int rc = conn.getResponseCode();
-//
-//        inputStream = conn.getInputStream();
+    public DependencyRatio() {
+        super();
+    }
 
-//        StringWriter writer = new StringWriter();
-//        IOUtils.copy(inputStream, writer);
-//        String theString = writer.toString();
-//        jsonResponse = readFromStream(inputStream);
-
-
-//        if (respon == 200) {
-//            return conn;
-//        } else {
-//            throw new java.io.IOException();
-//        }
-//    }
 
     public void getDependencyRatio() {
         try {
-//            HttpURLConnection conn = makeHttpConnection();
             score = (float) 1;
         } catch (Exception e) {
             score = (float) 0;
         }
+    }
 
+    public int isMajMinPinned(String s){
+        String exp = "";
+        System.out.println(s);
+        System.out.println(s.length());
+        if (s == "a") {
+            return 0;
+        }
+        return 1;
     }
 
 }
--- project-1-3/src/test/java/com/ECE461P1/app/scores/DependencyRatioTest.java
+++ project-1-3/src/test/java/com/ECE461P1/app/scores/DependencyRatioTest.java
@@ -1,20 +1,27 @@
 package com.ECE461P1.app.scores;
 import com.ECE461P1.app.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DependencyRatioTest {
 
-    @Test
-    void makeHttpConnection() {
-        Url url = new Url("https://github.com/jonschlinkert/even");
-        DependencyRatio dependRatio = new DependencyRatio(url.ownerName, url.repoName);
-        dependRatio.getDependencyRatio();
-        assertEquals(dependRatio.score, 1);
+
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dependencyTestData.csv", numLinesToSkip = 0)
+    void isMajMinPinned(String input, int expected) { //TODO: why does this not work with csv
+        DependencyRatio d = new DependencyRatio();
+        String str = "a";
+        System.out.println( str.strip() == input.strip());
+        assertEquals(d.isMajMinPinned(input), expected);
     }
 
     @Test
-    void getDependencyRatio() {
+    void isMajMinPinned1() {
+        DependencyRatio d = new DependencyRatio();
+        assertEquals(0, d.isMajMinPinned("a"));
     }
 }

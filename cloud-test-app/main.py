bf98ec57caf13a68557d49c7b950ab8a8bef301a
villar10 <60012351+villar10@users.noreply.github.com>
2021-11-15 19:54:43 -0500

    Updates to multiple functions
    - Upgraded App Engine Instance to F2 due to memory limit issues
    - Forced filenames to ensure Blob Storage and SQL storage can match
    - Updated API endpoints to allow for package uploading and full reset functionality
    - Upgraded JAR file from Owen
    - Updated requirements

--- cloud-test-app/main.py
+++ cloud-test-app/main.py
@@ -4,7 +4,6 @@ import sys
 import json
 import flask as fk
 import flask_restful as fkr
-from flask_restful import reqparse
 import logging
 import os
 from io import BytesIO
@@ -15,7 +14,6 @@ from google.cloud.storage import client
 from werkzeug.utils import redirect, secure_filename
 import pymysql
 import json
-#import main_api
 import base64
 import zipfile
 
@@ -26,11 +24,11 @@ api = fkr.Api(app)
 # Configure this environment variable via app.yaml
 CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
 MAX_RESULTS_PER_PAGE = 10
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip'}
+#ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip'}
+ALLOWED_EXTENSIONS = {'zip'}
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and (filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS)
 
 def round_up(dividend, divisor) -> int:
     output = int(dividend // divisor + (dividend % divisor > 0))
@@ -41,6 +39,11 @@ def parseJson(filename):
         data = json.load(json_file)
 
     return data
+
+def getMetadataByID(ID):    #Function should return metadata as JSON from given ID as string
+    #TODO metadata_JSON = sql_lookup
+    metadata_JSON = {"content": "in getMetadataByID"}
+    return metadata_JSON
 
 def connecttoDB():
     db_user = os.environ['CLOUD_SQL_USERNAME']
@@ -62,26 +65,37 @@ def connecttoDB():
     
     pass
 
+def format_json_string(json_as_string)->str:
+    jsonData = json.loads(json_as_string)
+    data = {}
+    data['repository'] = jsonData['repository']
+    data['dependencies'] = list(jsonData['dependencies'].values())
+    #return str(data)
+    return data         #Changed return, double casting as string unneccessary and adding "\" to output
+
+def makeIdentifiers(packagejson) -> str:
+    packagedict = json.loads(packagejson)
+    ID = str(packagedict.get('name')) + str(packagedict.get('version'))
+    name = str(packagedict.get('name'))
+    return ID, name
 
 @app.route('/')
 def homepage():
-
 
     return fk.render_template('root.html')
 
 @app.route('/reset')
 def reset():
 
-
     return fk.render_template('reset_warning.html')
 
 @app.route('/reset-confirmed', methods=["GET", "POST"])
 def resetConfirmed():
+    #TODO Make sure to remove SQL database items as well
     if fk.request.method == 'GET':
         return redirect('/')
     gcs = storage.Client()
 
-    # Get the bucket that the file will be downloaded from.
     bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
     blobs = [blob_new.name for blob_new in bucket.list_blobs()] # List all the blobs available
     for target_name in blobs:
@@ -102,15 +116,8 @@ def javaVersion():
     version1 = sys.platform
     return fk.render_template('javaversion.html', java_version1 = version1, java_version2 = version2, java_version3 = version3)
 
-@app.route('/hiddenpage')
-def rootSQRD():
-
-
-    return fk.render_template('root.html')
-
 @app.route('/upload-package')
 def uploadPackage():
-
 
     return fk.render_template('upload_package.html')
 
@@ -127,43 +134,11 @@ def downloadPackage():
     blobs = [blob_new.name for blob_new in bucket.list_blobs()] # List all the blobs available
     target_name =  blobs[get_package_index] #Get the blob name
     target_blob = bucket.blob(target_name)  #Get the right blob by filename
-    #print(target_blob)
-    #print(type(target_blob))
-    #file_to_download = target_blob.download_to_filename(target_name)
-    #print("file_to_download TYPE: ", type(file_to_download))
     bytes = target_blob.download_as_bytes()
-    #bytes.seek(0,0)
-    #print("FILE TYPE: ",type(file_to_download))
-    #print("BYTES TYPE: ",type(bytes))
-    #print("BYTES: \n\n",bytes)
     bytes_as_file = BytesIO(bytes)
-    #URL_to_download = 'https://storage.googleapis.com/ece-461-project-2-team-4.appspot.com/' + target_name
-    #print(URL_to_download)
 
-    
-    #return fk.render_template('download_package_success.html', URL_to_download=URL_to_download)
-    #return fk.render_template('download_package_success.html')
     return fk.send_file(bytes_as_file, as_attachment=True, attachment_filename=target_name)
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    #   dummy_times = [datetime.datetime(2000, 5, 25, 10, 0, 0),
-    #                  datetime.datetime(2000, 5, 25, 10, 30, 0),
-    #                  datetime.datetime(2000, 5, 25, 11, 0, 0),
-    #                 ]
-    '''conn = connecttoDB()
-    cursor = conn.cursor()
-    data = parseJson("output.json")
 
-    try:
-        for entry in data["scores"]:
-            cursor.execute("INSERT INTO scores_table (url, ramp_up_score, correctness_score, bus_factor_score, responsive_maintainer_score, license_score, dependency_score) VALUES(%s, %s, %s, %s, %s, %s, %s)", (entry["url"], entry["rampup"], entry["correctness"], entry["busfactor"], entry["contributors"], entry["license"], entry["dependency"]))
-
-        conn.commit()
-        pass
-    except:
-        pass'''
-    
-    #return render_template('upload_package.html')
 
 
 
@@ -182,9 +157,6 @@ def viewPackages(curr_page = 1):
 
     # Get the bucket that the file will be uploaded to.
     bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
-
-    #blob_iter = bucket.list_blobs(bucket)
-    #curr_page = 1                          #hard coded placeholder
 
     all_pages = bucket.list_blobs().pages
     total_blobs = 0
@@ -206,20 +178,6 @@ def viewPackages(curr_page = 1):
 
 
     return fk.render_template('view_packages.html', package_identifiers=package_identifiers, curr_page=curr_page, max_pages=max_pages)
-
-
-
-
-def format_json_string(json_as_string)->str:
-    jsonData = json.loads(json_as_string)
-    data = {}
-    data['repository'] = jsonData['repository']
-    data['dependencies'] = list(jsonData['dependencies'].values())
-    return str(data)
-
-
-
-
 
 
 @app.route('/upload', methods=['POST'])
@@ -261,16 +219,13 @@ def upload():
         names_in_zip = zip_file.namelist()
         for curr_file in names_in_zip:
             if curr_file.endswith('json') and ('package.json' in curr_file):
-                print(curr_file, "\n\n\n")
+                #print(curr_file, "\n\n\n")
                 zip_file_jsons.append(zip_file.open(curr_file).read())
-
-        print(type(zip_file_jsons[0]))
-        print(zip_file_jsons)
-        #print(zip_file_jsons[0].decode("utf-8"))
+        
         json_as_string = zip_file_jsons[0].decode("utf-8")
-        print(json_as_string)
-        print('BLANK LINES\n\n\n\n')
-
+        fileID, fileName = makeIdentifiers(json_as_string)
+        fileID = fileID + ".zip"
+        uploaded_file.filename = secure_filename(fileID)
         print("SUBPROCESS OUTPUT:   ", subprocess.run(["./Java_install/jdk-17.0.1/bin/java", "-jar", "trustworthiness_copy-1.0-SNAPSHOT-jar-with-dependencies.jar", format_json_string(json_as_string)], capture_output=True))
 
 
@@ -317,40 +272,148 @@ def server_error(e: Union[Exception, int]) -> str:
 
 
 
+class APIHome(fkr.Resource):
+    def get(self):
+
+        printout={
+            "description": "Welcome to the Package Manager API!", 
+            "Available Commands": {
+                "View Packages" : "/packages",
+                "Reset System" : "/reset",
+                "Upload Package" : "/package",
+                "Package by Name" : "/package/byName/{name}",
+                "Package by ID" : "/package/{id}",
+                "Package Rating by ID" : "/package/{id}/rate"
+            }
+        } 
+        return printout
+        
+    def options(self):
+        return 'Error in Options for APIHome', 400
+
+api.add_resource(APIHome, '/api')
 
 class UploadPackage(fkr.Resource):
     def post(self):
-        print("In POST")
-        some_json = fkr.request.get_json()
-        #some_json = reqparse.RequestParser().parse_args()
-        print(some_json)
-        print("HERE")
-        #print("REST ",fkr.request.get_json())
-        #print("FLASK ",fk.request.get_json())
-        print(type(some_json))
-        file_string = some_json['data']['Content']
-        print("FILE STRING: ", file_string)
-        #file_object = base64.b64encode(file_string.read()).decode('utf-8')
-        ##print(file_object)
-        #print(type(file_object))
-        return {'data': file_string}
+        #print("In POST")
+        #Read payload as json
+        input_json = fkr.request.get_json()
+        #Get file content as 64-bit encoded String
+        file_string = input_json['data']['Content']
+        file_ID_raw = str(input_json['metadata']['ID'])
+        file_ID = secure_filename(file_ID_raw + ".zip")#Set filename for storage on GCP (Storage and SQL)
+
+        decoded_data = base64.b64decode(bytes(file_string, 'utf-8'))    #Make into bytes
+
+        #Make zipfile object and grab 'package.json' TODO Add this to requirements
+        uploaded_file = BytesIO(decoded_data)
+        zip_file = zipfile.ZipFile(uploaded_file, "r")
+        zip_file_jsons = []
+        names_in_zip = zip_file.namelist()
+        for curr_file in names_in_zip:
+            if curr_file.endswith('json') and ('package.json' in curr_file):
+                zip_file_jsons.append(zip_file.open(curr_file).read())
+
+        json_as_string = zip_file_jsons[0].decode("utf-8")
+        print(json_as_string)
+
+        gcs = storage.Client()
+        # Get the bucket that the file will be uploaded to.
+        bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+        conn = connecttoDB()
+        cursor = conn.cursor()
+        data = parseJson("output.json")
+        try:
+            for entry in data["scores"]:
+                cursor.execute("INSERT INTO scores_table (url, ramp_up_score, correctness_score, bus_factor_score, responsive_maintainer_score, license_score, dependency_score) VALUES(%s, %s, %s, %s, %s, %s, %s)", (entry["url"], entry["rampup"], entry["correctness"], entry["busfactor"], entry["contributors"], entry["license"], entry["dependency"]))
+
+            conn.commit()
+            pass
+        except:
+            pass
+
+
+        # Create a new blob and upload the file's content.
+        blob = bucket.blob(uploaded_file.filename)
+
+        blob.upload_from_string(uploaded_file.read(),content_type=uploaded_file.content_type)
+
+        return {'data': json_as_string}
 
     def options(self):
-        print("In Options")
-        some_json1 = fkr.request.get_json()
-        some_json2 = reqparse.RequestParser().parse_args()
-        #print("REST ",fkr.request.get_json(force=True))
-        #print("FLASK ",fk.request.get_json(force=True))
-        print(type(some_json1))
-        print(some_json1)
-        print(type(some_json2))
-        print(some_json2)
-        file_string = some_json1['data']['Content']
-        file_object = base64.b64encode(file_string.read()).decode('utf-8')
-        print(file_object)
-        print(type(file_object))
+        return 'Error in Options for UploadPackage', 400
 
-api.add_resource(UploadPackage, '/api/upload')
+api.add_resource(UploadPackage, '/api/package')
+
+
+
+class ViewPackages(fkr.Resource):
+    def post(self, offset=1):
+
+        curr_page = offset
+
+        try:
+            curr_page = int(curr_page)
+        except:
+            curr_page = 1
+
+        gcs = storage.Client()
+
+
+        bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+        all_pages = bucket.list_blobs().pages
+        total_blobs = 0
+        for page in all_pages:
+            total_blobs += page.num_items
+        max_pages = round_up(total_blobs, MAX_RESULTS_PER_PAGE)
+        if (curr_page < 1):           #Should we deal with an upper bound?
+            curr_page = 1
+        elif(curr_page > max_pages):
+            curr_page = max_pages
+        max_results = MAX_RESULTS_PER_PAGE * curr_page
+        package_identifiers = []
+
+        for  index_blobs, blob in enumerate(bucket.list_blobs(max_results=max_results)):
+            if index_blobs >= ((curr_page-1) * MAX_RESULTS_PER_PAGE):
+                #print("Blob index is: ",index_blobs, " with name ", blob.name, " on page ", page)
+                
+                blob_tuple = (index_blobs, blob.name)
+                package_identifiers.append(blob_tuple)
+                #TODO Look up package metadata in SQL from package_identifier. This metadata is what will be returned in JSON format.
+
+        return {'description': 'in get ViewPackages'}
+
+    def options(self):
+        return 'Error in Options for UploadPackage', 400
+
+api.add_resource(ViewPackages, '/api/packages')
+
+
+
+class DeletePackages(fkr.Resource):
+    def delete(self):
+        try:
+            #TODO Make sure to remove SQL database items as well
+            gcs = storage.Client()
+
+            bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+            blobs = [blob_new.name for blob_new in bucket.list_blobs()] # List all the blobs available
+            for target_name in blobs:
+                target_blob = bucket.blob(target_name)  #Get the right blob by filename
+                target_blob.delete()
+
+
+
+            return {"description": "Registry is reset"}, 200
+        except:
+            return {"description": "Error in return"}, 400
+
+    def options(self):
+        return 'Error in Options for UploadPackage', 400
+
+api.add_resource(DeletePackages, '/api/reset')
+
 
 
 if __name__ == '__main__':

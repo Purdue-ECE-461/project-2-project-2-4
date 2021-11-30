c1954c9feda6ea465a0119857232236926574324
villar10 <60012351+villar10@users.noreply.github.com>
2021-11-29 23:47:01 -0500

    Updated Main

--- cloud-test-app/main.py
+++ cloud-test-app/main.py
@@ -13,17 +13,40 @@ from flask.helpers import url_for
 from google.cloud import storage
 from google.cloud.storage import client
 from werkzeug.utils import redirect, secure_filename
+from google.cloud import secretmanager
 
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
 # Configure this environment variable via app.yaml
 CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
+#APPLICATION_ID = os.environ['APPLICATION_ID']
 MAX_RESULTS_PER_PAGE = 10
 ALLOWED_EXTENSIONS = {'zip'}
+
+
+def access_secret_version(secret_id, version_id="latest"):
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the secret version.
+    name = f"projects/951373247679/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version.
+    response = client.access_secret_version(name=name)
+
+    # Return the decoded payload.
+    return response.payload.data.decode('UTF-8')
+os.environ["GITHUB_TOKEN"] = access_secret_version("GITHUB_TOKEN")
+
+
+def round_up(dividend, divisor) -> int:
+    """Simple function to round up an integer using logic"""
+    output = int(dividend // divisor + (dividend % divisor > 0))
+    return output
 
 def zipLogic(zip):
     #zip_IO = BytesIO(zip)
@@ -40,7 +63,16 @@ def zipLogic(zip):
 def format_json_string(json_as_string)->str:
     jsonData = json.loads(json_as_string)
     data = {}
-    data['repository'] = jsonData['repository']
+    url = ""
+    if type(jsonData['repository']) is dict:
+        url = jsonData['repository']['url']
+    else:
+        url = jsonData['repository']
+
+    url = url.replace("git@github.com:", "")
+    url = url.replace(".git", "")
+    data['repository'] = url
+
     try:
         data['dependencies'] = list(jsonData['dependencies'].values())
         data['dependencies'] = [s.replace("^", "") for s in data['dependencies']]
@@ -70,6 +102,62 @@ class APIHome(Resource):
         } 
         return printout
 
+class ViewPackages(Resource):
+    def __init__(self):
+            self.reqparse = reqparse.RequestParser()
+            self.reqparse.add_argument('offset', type = int, default=1)
+
+            
+    def post(self):
+        try:    
+            args = self.reqparse.parse_args()
+            offset = int(args['offset']) #Should be int given by user
+            curr_page = offset
+
+            try:
+                curr_page = int(curr_page)
+            except:
+                curr_page = 1
+
+            gcs = storage.Client()
+
+
+            bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+            all_pages = bucket.list_blobs().pages
+            total_blobs = 0
+            for page in all_pages:
+                total_blobs += page.num_items
+            max_pages = round_up(total_blobs, MAX_RESULTS_PER_PAGE)
+            if (curr_page < 1):           #Should we deal with an upper bound?
+                curr_page = 1
+            elif(curr_page > max_pages):
+                curr_page = max_pages
+            max_results = MAX_RESULTS_PER_PAGE * curr_page
+            package_identifiers = []
+
+            for  index_blobs, blob in enumerate(bucket.list_blobs(max_results=max_results)):
+                if index_blobs >= ((curr_page-1) * MAX_RESULTS_PER_PAGE):
+                    #print("Blob index is: ",index_blobs, " with name ", blob.name, " on page ", page)
+                    
+                    #blob_tuple = (index_blobs, blob.name)
+                    #package_identifiers.append(blob_tuple)
+                    fileName, fileID, fileVersion = blob.name.split('_') # get the metadata from the blob name
+                    #fileVersion = fileVersion.removesuffix('.zip') # remove .zip from fileversion  #TODO Discuss with Mohamamd why "removesuffix" doesnt exist -> this was added in 3.9, use replace
+                    fileVersion = fileVersion.replace(".zip", "")
+                    JsonIdentifier = {
+                    "Name": fileName,
+                    "Version": fileVersion,
+                    "ID": fileID
+                    }
+                    package_identifiers.append(JsonIdentifier)
+
+            return package_identifiers, 201 #May need a "json.dumps()" here for package_identifiers since it is a list of JSONs
+               
+        except:
+            return {"message": "Error in request."}, 400
+
+
 class UploadPackage(Resource):
     def post(self):
         try:
@@ -85,6 +173,11 @@ class UploadPackage(Resource):
             file_name = str(input_json['metadata']['Name'])
             file_ID = str(input_json['metadata']['ID'])
             file_version = str(input_json['metadata']['Version'])
+
+            file_ID = file_ID.replace("_", "")  #Ensure underscores are only used in the final filename
+            file_name = file_name.replace("_", "")
+
+            #TODO Test Rate to ensure > 0.5
 
             final_file_name = secure_filename(file_name + "_" + file_ID + "_" + file_version + ".zip") #Set filename for storage on GCP (Storage and SQL)
             decoded_data = base64.b64decode(bytes(file_string, 'utf-8')) #Make into bytes
@@ -152,7 +245,8 @@ class HandlePackageById(Resource):
                 if "_" + packageId + "_" in blob.name:
                     bytes_as_file = BytesIO(blob.download_as_bytes())
                     fileName, fileId, fileVersion = blob.name.split('_') # get the metadata from the blob name
-                    fileVersion = fileVersion.removesuffix('.zip') # remove .zip from fileversion
+                    #fileVersion = fileVersion.removesuffix('.zip') # remove .zip from fileversion
+                    fileVersion = fileVersion.replace(".zip", "")
 
                     packageContent = base64.b64encode(bytes_as_file.read())
                     packageContentString = packageContent.decode('ascii')
@@ -235,10 +329,11 @@ class Rates(Resource):
                     bytes_as_file = BytesIO(blob.download_as_bytes())
                     json_as_string = zipLogic(bytes_as_file)
 
-                    output = subprocess.run(["java", "-jar", "trustworthiness_copy-1.0-SNAPSHOT-jar-with-dependencies.jar", format_json_string(json_as_string)], capture_output=True)
+                    #output = subprocess.run(["java", "-jar", "trustworthiness_copy-1.0-SNAPSHOT-jar-with-dependencies.jar", format_json_string(json_as_string)], capture_output=True)
 
                     # use this if you are deploying
-                    # output = subprocess.run(["./Java_install/jdk-17.0.1/bin/java", "-jar", "trustworthiness_copy-1.0-SNAPSHOT-jar-with-dependencies.jar", format_json_string(json_as_string)], capture_output=True)
+                    output = subprocess.run(["./Java_install/jdk-17.0.1/bin/java", "-jar", "trustworthiness_copy-1.0-SNAPSHOT-jar-with-dependencies.jar", format_json_string(json_as_string)], capture_output=True)
+                    print(output)
                     output_json =  output.stdout.decode("utf-8")
                     data = json.loads(output_json)['Scores'][0]
 
@@ -254,7 +349,7 @@ class Rates(Resource):
 
             return {"message":"No such package exists."}, 400
         except:
-            return {"message":"The package rating system chocked on at least one of the metrics."}, 500
+            return {"message":"The package rating system choked on at least one of the metrics."}, 500
 
 class RegistryReset(Resource):
     def delete(self):
@@ -272,11 +367,14 @@ class RegistryReset(Resource):
             return {"description": "Error in return"}, 400
 
 api.add_resource(APIHome, '/')
+api.add_resource(ViewPackages, '/packages')
 api.add_resource(UploadPackage, '/package')
 api.add_resource(HandlePackageByName, '/package/byName/<string:packageName>')
 api.add_resource(HandlePackageById, '/package/<string:packageId>')
 api.add_resource(Rates, '/package/<string:packageId>/rate')
 api.add_resource(RegistryReset, '/reset')
+
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)

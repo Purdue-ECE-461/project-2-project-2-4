e776d08944cd4ffd7e39452759ebc2e9ba7c64ce
villar10 <60012351+villar10@users.noreply.github.com>
2021-12-08 20:03:17 -0500

    Cleaned house of old test files

--- cloud-test-app/zipfiletest.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-fin = open('project-2-starter-4.zip', 'rb')
-fin_read = fin.read()
-zipasbytes = base64.b64encode(fin_read)
-fin.close()
-print(zipasbytes)
-print(type(zipasbytes))
--- yaml_testing.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-if __name__ == '__main__':
-
-    stream = open("ECE_461_Fall_2021_Project_2_spec_v1.0.yaml", 'r')
-    dictionary = yaml.load(stream)
-    for key, value in dictionary.items():
-        print (key + " : " + str(value))
